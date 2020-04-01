d14c2a22f35d52f648eadcf736914133584c82fd
Afonso Muralha <afonsomuralha@gmail.com>
2020-04-01 12:25:27 +0100

    updated lab3

--- lab3/1.py
+++ lab3/1.py
@@ -7,23 +7,38 @@ from sklearn.preprocessing import StandardScaler
 from datetime import datetime
 
 
-df = pd.read_csv('EURUSD_Daily_Ask_2018.12.31_2019.10.05v2.csv', na_values=['.'], sep=r'\s*,\s*', delimiter=';')
+df = pd.read_csv('EURUSD_Daily_Ask_2018.12.31_2019.10.05v2.csv', na_values=['.'], sep=r'\s*,\s*', delimiter=';', decimal=',')
+df.replace(0, np.nan, inplace=True)
 
-# df = df.dropna() #without this, the rolling average is a mess because of the NaN values
-print(df)
-
-# df["Time (UTC)"] = df["Time (UTC)"].map(lambda x: datetime.strptime(str(x), '%Y.%m.%d %H:%M:%S'))
 df['Time (UTC)'] = pd.to_datetime(df['Time (UTC)'], format='%Y.%m.%d %H:%M:%S')
 x = df['Time (UTC)']
-Open = df['Open']
-High = df['High']
-Low = df['Low']
-Close = df['Close']
-Volume = df['Volume ']
+Open = df['Open'].fillna(df['Open'].mean(skipna=True))
+High = df['High'].fillna(df['High'].mean(skipna=True))
+Low = df['Low'].fillna(df['Low'].mean(skipna=True))
+Close = df['Close'].fillna(df['Close'].mean(skipna=True))
+Volume = df['Volume '].fillna(df['Volume '].mean(skipna=True))
 # df['DCOILBRENTEU'] = df['DCOILBRENTEU'].astype(float)
 # y = df[['DCOILBRENTEU']]
 
 
+def detect_outliers(x, y, k):
+    avg = y.mean()
+    std_dev = y.values.std(ddof=1)
+    print("Average: " + str(avg) + "\n")
+    print("Std dev: " + str(std_dev) + "\n")
+    down = y[(y < avg - k * std_dev)]
+    up = y[(y > avg + k * std_dev)]
+    print("Outliers below average-K*sigma\n")
+    print(down)
+    print("Outliers above average+K*sigma\n")
+    print(up)
+    print("All outliers \n")
+    # all = all.concat
+    print(up.append(down))
+
+
+
+    # print(std_dev)
 
 plt.subplot(141)
 plt.plot(x, Open)
@@ -34,6 +49,7 @@ plt.plot(x, Low)
 plt.subplot(144)
 plt.plot(x, Close)
 
+detect_outliers(x, Open, 1)
 
 # plt.subplot(142)
 # plt.plot(x, y_minmax)
